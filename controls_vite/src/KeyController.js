3c2d61e0fb5fc58ca38edeb76dbea4a04c561a02
Darock <heetask@gmail.com>
2026-02-07 13:56:41 +0900

    마인크래프트 스타일 컨트롤

---
+++ controls_vite/src/KeyController.js
@@ -0,0 +1,15 @@
+export class KeyController {
+  constructor() {
+    this.keys = [];
+
+    window.addEventListener("keydown", (e) => {
+      this.keys[e.code] = true; // e.code: 'KeyW' 등
+      console.log(e.code + "누름");
+    });
+
+    window.addEventListener("keyup", (e) => {
+      delete this.keys[e.code];
+      console.log(e.code + "땜");
+    });
+  }
+}
